a231a18defa34759acee0005df75929b63841361
Shivam Maheshwari <theshivammaheshwari@gmail.com>
2025-05-29 16:45:41 +0530

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -74,13 +74,9 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
- 'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'student_db',
-        'USER': 'student_user',
-        'PASSWORD': 'password',
-        'HOST': 'localhost',
-        'PORT': '3306',
+  'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / "db.sqlite3",
     }
 }
 
